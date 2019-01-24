b687a4b15706893276b744f178165e13ab6af8e8
RaquelCC <rcanalesconcha@gmail.com>
2019-01-23 23:22:33 -0300

    uso de Promise.all() para solucionar problemas de orden de ejecución de funciones // Firebase con reglas de autenticación y validación, acceso a listas funcionando como corresponde

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+
+ 
+
 document.addEventListener("DOMContentLoaded", event => {
     window.M.AutoInit();
+    let instance = M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'),
+        {
+                hover: true,
+                constrainWidth: false,
+                coverTrigger: false,
+                closeOnClick: false
+            }
+    )
     let config = {
         apiKey: "AIzaSyBTscKCGyacpck5Wdb7qpKySQ_6qR-ppBk",
         authDomain: "primera-hackaton-17f7d.firebaseapp.com",
@@ -21,13 +32,14 @@ document.addEventListener("DOMContentLoaded", event => {
                 saveNewUser();
             }
         })
-
+        document.getElementById("nav-logout").style.display = "none";
+        document.getElementById("nav-login").style.display = "block";
         document.getElementById("new-user-mail").value = "";
         document.getElementById("new-user-password").value = "";
         document.getElementById("user-mail").value = "";
         document.getElementById("user-password").value = "";
         document.getElementById("root").innerHTML = `<p>Hola ${user.email}</p>
-          <button id="logout" type="button">Log Out</button>
+    
           `
         document.getElementById("logout").addEventListener("click", ()=> {
             firebase.auth().signOut().then(function() {
@@ -38,11 +50,13 @@ document.addEventListener("DOMContentLoaded", event => {
           })
         } else {
           // No user is signed in.
+        document.getElementById("nav-logout").style.display = "block";
+        document.getElementById("nav-login").style.display = "none";
         document.getElementById("new-user-mail").value = "";
         document.getElementById("new-user-password").value = "";
         document.getElementById("user-mail").value = "";
         document.getElementById("user-password").value = "";
-        document.getElementById("root").innerHTML = ``
+        document.getElementById("root").innerHTML = ""
         }
       });
     
@@ -67,6 +81,7 @@ document.addEventListener("DOMContentLoaded", event => {
             var errorCode = error.code;
             var errorMessage = error.message;
             // ...
+            alert(errorCode+ " " + errorMessage)
             
         });
         
@@ -75,20 +90,23 @@ document.addEventListener("DOMContentLoaded", event => {
 
     function saveNewUser(){
         database.ref("users/"+firebase.auth().currentUser.uid).set({
-            email: firebase.auth().currentUser.email
+            email: firebase.auth().currentUser.email,
+        })
+        database.ref("users/"+firebase.auth().currentUser.uid).update({
+            movieLists: false
         })
     }
-
-    let pageCounter = 0;
-    do {
-        pageCounter++;
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=48819a4f88e3d597df63bebab6723d0f&primary_release_year=2019&page="+pageCounter)
-            .then(data => data.json())
-            .then(data => {
+   
+    
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=48819a4f88e3d597df63bebab6723d0f&primary_release_year=2019")
+        .then(data => data.json())
+        .then(data => {
+            let promises = [];
                 let dataMovies = data.results;
                 dataMovies.forEach(movie => {
                     let title = movie.title;
-                    fetch("https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?t="+title+"&y=2019&apikey=c39cba8d") //9513ffad
+                    moviesShown = [];
+                    promises.push(fetch("https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?t="+title+"&plot=full&y=2019&apikey=8b8cc00a") //9513ffad //c39cba8d  // 8b8cc00a
                         .then(data => data.json())
                         .then(data => {
                             if (data.Response === "False"){
@@ -97,14 +115,15 @@ document.addEventListener("DOMContentLoaded", event => {
                             if (data.Poster === "N/A"){
                                 return;
                             }
+                            moviesShown.push(data)
                             document.getElementById("movies").innerHTML += `
                             <div class="col s12 m4 movie-card">
                                 <div class="card small center-align white">
                                     <div class="card-content white-text">
-                                        <img class="responsive-img" src="${data.Poster}">
+                                        <img class="responsive-img card-poster" src="${data.Poster}">
                                     </div>
                                     <div class="card-action">
-                                        <a href="#">${data.Title}</a>
+                                        <a class="individual-movie-link orange-text text-darken-4" href="#">${data.Title}</a>
                                     </div>
                                 </div>
                             </div>
@@ -112,24 +131,138 @@ document.addEventListener("DOMContentLoaded", event => {
                             `
     
                             
-                        })
-                })
+                        }))
+                }) 
+                
+                return Promise.all(promises)
             })
-    } while (pageCounter < 10)
-
-
-
-
+            .then(()=>{
+                console.log("cargo")
+                createLinks(movieLinks);
+            })
+            
+            
+            
 
     
 
+    document.getElementById("search").addEventListener("click", ()=> {
+        let promises = [];
+        document.getElementById("legend").style.display = "none";
+        document.getElementById("movies").innerHTML = "";        
+        document.getElementById("movies").style.display = "block";
+        document.getElementById("movies-individual").style.display = "none";
+        let userSearch = document.getElementById("search-input").value;
+        function displaySearch(){
+            promises.push(fetch("https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?t="+userSearch+"&plot=full&apikey=8b8cc00a") // 9513ffad //c39cba8d // 8b8cc00a
+                            .then(data => data.json())
+                            .then(data => {
+                                // console.log("oli")
+                                if (data.Response === "False"){
+                                    return;
+                                }
+                                if (data.Poster === "N/A"){
+                                    return;
+                                }
+                                moviesShown = [];
+                                moviesShown.push(data)
+                                document.getElementById("movies").innerHTML += `
+                                    <div class="col s12 m4 movie-card">
+                                        <div class="card small center-align white">
+                                            <div class="card-content white-text">
+                                                <img class="responsive-img card-poster" src="${data.Poster}">
+                                            </div>
+                                            <div class="card-action">
+                                                <a class="individual-movie-search" href="#">${data.Title}</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                    `
+    
+                                }))
+                                return Promise.all(promises);
+
+        }
+        displaySearch().then(()=>{
+            console.log("cargo")
+            let searchLinks = document.getElementsByClassName("individual-movie-search");
+            createLinks(searchLinks);
+        })
+                            
+
+                            
+                            
+                        
+
+    })
+    
+
+    
+
+    let moviesShown = [];
 
 
+    let movieLinks = document.getElementsByClassName("individual-movie-link");
+    function createLinks(movieLinks) {
+    
+        for(let i = 0; i<movieLinks.length; i++) {
+            movieLinks[i].addEventListener("click", () => {
+                document.getElementById("legend").style.display = "none";
+                document.getElementById("movies").style.display = "none";
+                document.getElementById("movies-individual").style.display = "block";
+                document.getElementById("movies-individual").innerHTML = `
+                <div class="col s5">
+                <img src=${moviesShown[i].Poster} class="responsive-img">
+                </div>
+                <div class="col s7">
+                <h4 class="sinopsis">Sinopsis</h4>
+                <p>${moviesShown[i].Plot}</p>
+                </div>
+                <div class="col s7">
+                <h4 class="sinopsis">Generos</h4>
+                <p>${moviesShown[i].Genre}</p>
+                </div>
+                <div class="col s7">
+                <h4 class="sinopsis">Premios</h4>
+                <p>${moviesShown[i].Awards}</p>
+                </div>
+                <div class="col s7">
+                <h4 class="sinopsis">IMDb</h4>
+                <p><a href="https://www.imdb.com/title/${moviesShown[i].imdbID}" target="_blank">Ficha en IMDb</a></p>
+                </div>
+                `
+            })
+        }
+    
+    }
+
+    document.getElementById("create-list").addEventListener("click", ()=> {
+        document.getElementById("legend").style.display = "none";
+        document.getElementById("movies").style.display = "none";
+        document.getElementById("movies-individual").style.display = "none";
+        document.getElementById("list-create-section").innerHTML = `
+        <div class="col s12">
+        <input id="list-name" type="text" placeholder="Nombre de la Lista..">
+        <button class="btn red" id="new-list">Crear</button>
+        </div>
+        `
+
+        document.getElementById("new-list").addEventListener("click", ()=> {
+            console.log("funciona boton listas")
+            let listName = document.getElementById("list-name").value
+            let saveList = [(database.ref("users/"+firebase.auth().currentUser.uid+"/movieLists").update({
+                [listName]: "a"
+            }))]
+            Promise.all(saveList).then(()=>{
+            document.getElementById("list-create-section").innerHTML = "Lista creada."
+        })
+        })
+        
+    })
 
 
-
-
-
+    
 
 
 
@@ -138,3 +271,5 @@ document.addEventListener("DOMContentLoaded", event => {
 
 
 })
+
+
